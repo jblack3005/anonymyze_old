bf8dd755114fe3a657049a59e758feab64f21df0
James <jbb2000@hotmail.co.uk>
2022-01-19 03:07:50 +0000

    First commit

---
+++ anonymyze/MainPage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace anonymyze
+{
+    public class MainPage : ContentPage
+    {
+        Button uploadButton;
+
+        public MainPage()
+        {
+            this.Padding = new Thickness(20, 20, 20, 20);
+
+            StackLayout panel = new StackLayout
+            {
+                Spacing = 15
+            };
+
+            panel.Children.Add(uploadButton = new Button
+            {
+                Text = "Upload",
+                IsEnabled = false,
+            });
+
+            uploadButton.Clicked += OnUpload;
+            this.Content = panel;
+        }
+
+        private void OnUpload(object sender, EventArgs e)
+        {
+            uploadButton.Text = "Image Uploaded";
+        }
+    }
+}
